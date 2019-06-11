c47d6ab8a16bc726d7f4ed58fbdb5c7842757c2c
Chris Kent <cjkent@hotmail.com>
2019-06-11 21:36:48 +0100

    Add more components to the Lumigo example

--- lumigo/core/src/main/kotlin/ws/osiris/example/lumigo/core/ApiDefinition.kt
+++ lumigo/core/src/main/kotlin/ws/osiris/example/lumigo/core/ApiDefinition.kt
@@ -3,24 +3,25 @@ package ws.osiris.example.lumigo.core
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.dynamodbv2.model.AttributeValue
-import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.sqs.AmazonSQS
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder
+import com.google.gson.Gson
 import io.lumigo.handlers.LumigoRequestExecutor
 import ws.osiris.aws.lambdaContext
 import ws.osiris.aws.lambdaEvent
 import ws.osiris.core.ComponentsProvider
 import ws.osiris.core.HttpHeaders
+import ws.osiris.core.HttpMethod
 import ws.osiris.core.api
-import java.util.UUID
+import java.util.*
 
-private const val ITEMS_TABLE: String = "Items"
-private const val ID: String = "id"
-private const val VALUE: String = "value"
+internal const val ITEMS_TABLE: String = "Items"
+internal const val ID: String = "id"
+internal const val VALUE: String = "value"
+internal const val ACTION: String = "action"
 
 /** The bucket name is a parameter in `root.template` that is passed to the generated CloudFormation file. */
-private val BUCKET_NAME = System.getenv("BucketName")
+internal val BUCKET_NAME = System.getenv("BucketName")
 
 /** The queue URL is a parameter in `root.template` that is passed to the generated CloudFormation file. */
 private val QUEUE_URL = System.getenv("QueueUrl")
@@ -38,12 +39,17 @@ val api = api<LumigoExampleComponents> {
         val items = dynamoClient.scan(ITEMS_TABLE, listOf(ID, VALUE)).items
         items.map { item(it) }
     }
+
     post("/values") { req ->
         val id = UUID.randomUUID().toString()
         val value = req.requireBody(String::class)
         dynamoClient.putItem(ITEMS_TABLE, mapOf(ID to AttributeValue(id), VALUE to AttributeValue(value)))
+        val messageBodyMap = mapOf(ID to id, ACTION to HttpMethod.PUT.name)
+        val messageBody = gson.toJson(messageBodyMap)
+        sqsClient.sendMessage(QUEUE_URL, messageBody)
         req.responseBuilder().status(201).header(HttpHeaders.LOCATION, "/values/$id").build()
     }
+
     get("/values/{id}") { req ->
         val id = req.pathParams[ID]
         val item = dynamoClient.getItem(ITEMS_TABLE, mapOf(ID to AttributeValue(id))).item
@@ -53,10 +59,18 @@ val api = api<LumigoExampleComponents> {
             item(item)
         }
     }
+
     delete("/values/{id}") { req ->
         val id = req.pathParams[ID]
         dynamoClient.deleteItem(ITEMS_TABLE, mapOf(ID to AttributeValue(id)))
+        val messageBodyMap = mapOf(ID to id, ACTION to HttpMethod.DELETE.name)
+        val messageBody = gson.toJson(messageBodyMap)
+        sqsClient.sendMessage(QUEUE_URL, messageBody)
         req.responseBuilder().status(204).build()
+    }
+
+    post("/cleanup") {
+        // TODO read all the values from dynamo, send DELETE messages, then delete from dynamo
     }
 }
 
@@ -73,8 +87,8 @@ fun createComponents(): LumigoExampleComponents = LumigoExampleComponentsImpl()
  */
 interface LumigoExampleComponents : ComponentsProvider {
     val dynamoClient: AmazonDynamoDB
-    val s3Client: AmazonS3
     val sqsClient: AmazonSQS
+    val gson: Gson
 }
 
 /**
@@ -82,11 +96,6 @@ interface LumigoExampleComponents : ComponentsProvider {
  */
 class LumigoExampleComponentsImpl : LumigoExampleComponents {
     override val dynamoClient: AmazonDynamoDB = AmazonDynamoDBClientBuilder.defaultClient()
-    override val s3Client: AmazonS3 = AmazonS3ClientBuilder.defaultClient()
     override val sqsClient: AmazonSQS = AmazonSQSClientBuilder.defaultClient()
+    override val gson: Gson = Gson()
 }
-
-// TODO a stand-alone lambda function that receives values over SQS and writes them to S3
-//   * ID is sent over SQS, lambda reads the value from Dynamo and stores in S3
-//   * SQS message includes an action "PUT" or "DELETE"
-// TODO a /cleanup endpoint that deletes everything from dynamo, S3 and SQS. so the stack can be deleted
--- lumigo/core/src/main/kotlin/ws/osiris/example/lumigo/core/SqsLambda.kt
+++ lumigo/core/src/main/kotlin/ws/osiris/example/lumigo/core/SqsLambda.kt
@@ -1,2 +1,58 @@
 package ws.osiris.example.lumigo.core
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.model.AttributeValue
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.events.SQSEvent
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.google.gson.Gson
+import io.lumigo.handlers.LumigoRequestHandler
+import org.slf4j.LoggerFactory
+import ws.osiris.core.HttpMethod
+
+private val log = LoggerFactory.getLogger("ws.osiris.example.lumigo.core")
+
+/**
+ * Lambda function that receives SQS messages and writes or deletes values from S3.
+ *
+ * The SQS message body is JSON with this format:
+ *
+ *     { "id": "...", "action", "..." }
+ *
+ * The action is `PUT` or `DELETE`.
+ *
+ * If the action is `PUT` the value is read from DynamoDB using the ID and written to S3 using the ID as the key.
+ *
+ * If the action is `DELETE` the value is deleted from S3; the ID is the key.
+ */
+class SqsLambda : LumigoRequestHandler<SQSEvent, Unit>() {
+
+    private val gson: Gson = Gson()
+    private val dynamoClient: AmazonDynamoDB = AmazonDynamoDBClientBuilder.defaultClient()
+    private val s3Client: AmazonS3 = AmazonS3ClientBuilder.defaultClient()
+
+    override fun doHandleRequest(event: SQSEvent, context: Context) {
+        val message = event.records[0]
+        val messageBodyMap = gson.fromJson(message.body, Map::class.java)
+        val id = messageBodyMap.getValue(ID) as String
+        val action = messageBodyMap.getValue(ACTION) as String
+        log.info("Received message with ID {}, action {}", id, action)
+        val result = dynamoClient.getItem(ITEMS_TABLE, mapOf(ID to AttributeValue(id)))
+            val value = result.item.getValue(VALUE).s
+        when (HttpMethod.valueOf(action)) {
+            HttpMethod.PUT -> putValue(id, value)
+            HttpMethod.DELETE -> deleteValue(id)
+            else -> throw IllegalArgumentException("Unexpected action $action")
+        }
+    }
+
+    private fun putValue(id: String, value: String) {
+        s3Client.putObject(BUCKET_NAME, id, value)
+    }
+
+    private fun deleteValue(id: String) {
+        s3Client.deleteObject(BUCKET_NAME, id)
+    }
+}

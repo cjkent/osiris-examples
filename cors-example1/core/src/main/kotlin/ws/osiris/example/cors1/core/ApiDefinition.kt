80a2eccaa23c921dbc17ad99cebad964fe0609a0
Chris Kent <cjkent@hotmail.com>
2020-03-03 21:33:05 +0000

    Add CORS example projects

---
+++ cors-example1/core/src/main/kotlin/ws/osiris/example/cors1/core/ApiDefinition.kt
@@ -0,0 +1,73 @@
+package ws.osiris.example.cors1.core
+
+import ws.osiris.core.MimeTypes
+import ws.osiris.core.ComponentsProvider
+import ws.osiris.core.HttpHeaders
+import ws.osiris.core.api
+
+/**
+ * The name of an environment variable used to pass in configuration.
+ *
+ * The values of environment variables can be specified by the `environmentVariables` property in the configuration:
+ *
+ *     val config = ApplicationConfig(
+ *         environmentVariables = mapOf(
+ *             "EXAMPLE_ENVIRONMENT_VARIABLE" to "Bob"
+ *         )
+ *         ...
+ *     )
+ */
+const val EXAMPLE_ENVIRONMENT_VARIABLE = "EXAMPLE_ENVIRONMENT_VARIABLE"
+
+/** The API. */
+val api = api<ExampleComponents> {
+
+    get("/helloworld") {
+        // return a map that is automatically converted to JSON
+        mapOf("message" to "hello, world!")
+    }
+
+    get("/helloplain") { req ->
+        // return a response with customised headers
+        req.responseBuilder()
+            .header(HttpHeaders.CONTENT_TYPE, MimeTypes.TEXT_PLAIN)
+            .build("hello, world!")
+    }
+
+    get("/helloqueryparam") { req ->
+        // get a query parameter
+        val name = req.queryParams["name"]
+        mapOf("message" to "hello, $name!")
+    }
+
+    get("/helloenv") {
+        // use the name property from ExampleComponents for the name
+        mapOf("message" to "hello, $name!")
+    }
+
+    get("/hello/{name}") { req ->
+        // get a path parameter
+        val name = req.pathParams["name"]
+        mapOf("message" to "hello, $name!")
+    }
+}
+
+/**
+ * Creates the components used by the example API.
+ */
+fun createComponents(): ExampleComponents = ExampleComponentsImpl()
+
+/**
+ * A trivial set of components that exposes a simple property to the request handling code in the API definition.
+ */
+interface ExampleComponents : ComponentsProvider {
+    val name: String
+}
+
+/**
+ * An implementation of `ExampleComponents` that uses an environment variable to populate its data.
+ */
+class ExampleComponentsImpl : ExampleComponents {
+    override val name: String = System.getenv(EXAMPLE_ENVIRONMENT_VARIABLE) ?:
+        "[Environment variable EXAMPLE_ENVIRONMENT_VARIABLE not set]"
+}
---
+++ cors-example2/local-server/src/main/kotlin/ws/osiris/example/cors2/localserver/Main.kt
@@ -0,0 +1,22 @@
+package ws.osiris.example.cors2.localserver
+
+import com.beust.jcommander.JCommander
+import ws.osiris.localserver.ServerArgs
+import ws.osiris.localserver.runLocalServer
+
+import ws.osiris.example.cors2.core.api
+import ws.osiris.example.cors2.core.createComponents
+
+/**
+ * Main function for running the application in an HTTP server (Jetty).
+ *
+ * The arguments are:
+ *   * `-p`, `--port` - the port the server listens on. Defaults to 8080.
+ *   * `-r`, `--root` - the context root. This is the base path from which everything is served.
+ */
+fun main(args: Array<String>) {
+    val serverArgs = ServerArgs()
+    JCommander.newBuilder().addObject(serverArgs).build().parse(*args)
+    val components = createComponents()
+    runLocalServer(api, components, serverArgs.port, serverArgs.root, "core/src/main/static")
+}

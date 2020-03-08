72643e81b657af4ad4844b4bc5f262ea06ac4e56
Chris Kent <cjkent@hotmail.com>
2020-03-08 20:02:46 +0000

    Add cors block to API

--- cors-example1/core/src/main/kotlin/ws/osiris/example/cors1/core/ApiDefinition.kt
+++ cors-example1/core/src/main/kotlin/ws/osiris/example/cors1/core/ApiDefinition.kt
@@ -1,6 +1,8 @@
 package ws.osiris.example.cors1.core
 
 import ws.osiris.core.ComponentsProvider
+import ws.osiris.core.CorsHeaders
+import ws.osiris.core.HttpMethod
 import ws.osiris.core.api
 
 /** The API. */
@@ -12,6 +14,14 @@ val api = api<ComponentsProvider>(cors = true) {
 
     post("/bar") {
         mapOf("message" to "hello, world!")
+    }
+
+    cors {
+        CorsHeaders(
+            allowHeaders = setOf(),
+            allowMethods = setOf(HttpMethod.GET, HttpMethod.POST),
+            allowOrigin = setOf("*")
+        )
     }
 }
 

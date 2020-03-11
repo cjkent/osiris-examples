7ba458bca22d82ea29244adf984cbe91840c26c2
Chris Kent <cjkent@hotmail.com>
2020-03-11 22:09:50 +0000

    More comprehensive example

--- cors-example1/core/src/main/kotlin/ws/osiris/example/cors1/core/ApiDefinition.kt
+++ cors-example1/core/src/main/kotlin/ws/osiris/example/cors1/core/ApiDefinition.kt
@@ -1,12 +1,13 @@
 package ws.osiris.example.cors1.core
 
+import com.google.gson.Gson
 import ws.osiris.core.ComponentsProvider
 import ws.osiris.core.HttpHeaders
 import ws.osiris.core.HttpMethod
 import ws.osiris.core.api
 
 /** The API. */
-val api = api<ComponentsProvider>(cors = true) {
+val api = api<CorsExample1Components>(cors = true) {
 
     cors {
         allowMethods = setOf(HttpMethod.GET, HttpMethod.POST)
@@ -18,8 +19,11 @@ val api = api<ComponentsProvider>(cors = true) {
         mapOf("message" to "hello, foo!")
     }
 
-    post("/bar") {
-        mapOf("message" to "hello, bar!")
+    post("/bar") { req ->
+        val json = req.body<String>()
+        val bodyMap = gson.fromJson(json, Map::class.java)
+        val name = bodyMap["name"]
+        mapOf("message" to "hello, $name!")
     }
 
     get("/baz", cors = false) {
@@ -28,6 +32,17 @@ val api = api<ComponentsProvider>(cors = true) {
 }
 
 /**
- * Creates the components used by the example API.
+ * Creates the components used by the API.
  */
-fun createComponents(): ComponentsProvider = object : ComponentsProvider {}
+fun createComponents(): CorsExample1Components = CorsExample1ComponentsImpl()
+
+/**
+ * Simple components that only provides a [Gson] instance.
+ */
+interface CorsExample1Components : ComponentsProvider {
+    val gson: Gson
+}
+
+internal class CorsExample1ComponentsImpl : CorsExample1Components {
+    override val gson: Gson = Gson()
+}

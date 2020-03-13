e786f83ee0bc71434c1bfeeb1d4e5d4e62f29263
Chris Kent <cjkent@hotmail.com>
2020-03-13 07:54:32 +0000

    Only have 1 CORS example project

---
+++ cors-example/local-server/src/main/kotlin/ws/osiris/example/cors/localserver/Main.kt
@@ -0,0 +1,21 @@
+package ws.osiris.example.cors.localserver
+
+import com.beust.jcommander.JCommander
+import ws.osiris.example.cors.core.api
+import ws.osiris.example.cors.core.createComponents
+import ws.osiris.localserver.ServerArgs
+import ws.osiris.localserver.runLocalServer
+
+/**
+ * Main function for running the application in an HTTP server (Jetty).
+ *
+ * The arguments are:
+ *   * `-p`, `--port` - the port the server listens on. Defaults to 8080.
+ *   * `-r`, `--root` - the context root. This is the base path from which everything is served.
+ */
+fun main(args: Array<String>) {
+    val serverArgs = ServerArgs()
+    JCommander.newBuilder().addObject(serverArgs).build().parse(*args)
+    val components = createComponents()
+    runLocalServer(api, components, serverArgs.port, serverArgs.root, "core/src/main/static")
+}

289f25058136ee73ebfcab6a25c1da44732d04b2
Chris Kent <cjkent@hotmail.com>
2020-05-02 14:36:13 +0100

    Create user pool, client and domain

--- cognito/core/src/main/kotlin/ws/osiris/example/cognito/core/Config.kt
+++ cognito/core/src/main/kotlin/ws/osiris/example/cognito/core/Config.kt
@@ -11,27 +11,11 @@ val config = ApplicationConfig(
     applicationName = "osiris-cognito-example",
     lambdaMemorySizeMb = 512,
     lambdaTimeout = Duration.ofSeconds(10),
-    environmentVariables = mapOf(
-        "EXAMPLE_ENVIRONMENT_VARIABLE" to "Bob"
-    ),
     stages = listOf(
         Stage(
             name = "dev",
             description = "Development stage",
-            deployOnUpdate = true,
-            variables = mapOf(
-                "VAR1" to "devValue1",
-                "VAR2" to "devValue2"
-            )
-        ),
-        Stage(
-            name = "prod",
-            description = "Production stage",
-            deployOnUpdate = false,
-            variables = mapOf(
-                "VAR1" to "prodValue1",
-                "VAR2" to "prodValue2"
-            )
+            deployOnUpdate = true
         )
     )
 )
